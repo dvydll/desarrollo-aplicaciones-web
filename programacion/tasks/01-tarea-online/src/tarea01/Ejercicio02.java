f6c29e7d1d68b07bc422f42a20dd337a2de1f4f4
dvyd <david.llopislaguna@gmail.com>
2024-10-01 20:04:23 +0200

    [DAW-PRGR-U1] Ejercicios 2 y 3

--- programacion/tasks/01-tarea-online/src/tarea01/Ejercicio02.java
+++ programacion/tasks/01-tarea-online/src/tarea01/Ejercicio02.java
@@ -1,10 +1,9 @@
-
 package tarea01;
 
 /**
  * Ejercicio 2. Operaciones con constantes y variables.
  *
- * @author  David Llopis Laguna
+ * @author David Llopis Laguna
  */
 public class Ejercicio02 {
 
@@ -18,9 +17,7 @@ public class Ejercicio02 {
         final int CONSTANTE2 = 3;
 
         // Variables de entrada
-        
         // Variables de salida
-        
         // Variables auxiliares
         boolean valorBool;
         byte enteroByte;
@@ -41,13 +38,10 @@ public class Ejercicio02 {
         //----------------------------------------------
         //     Procesamiento 
         //----------------------------------------------
-        
         //----------------------------------------------
         // Ejemplos que se proporcionan como modelo:
-        
         // Las variables booleanas solo pueden tener los valores true o false
         // valorBool = 0;
-        
         // decimalSimple = 9.9 * 4.6;       
         // SOLUCIÓN: CASTING EXPLÍCITO
         decimalSimple = (float) (9.9 * 4.6);
@@ -56,28 +50,44 @@ public class Ejercicio02 {
         enteroInt = 'a';
         //----------------------------------------------
 
-        decimalDoble = 5,17;
-        
+        // decimalDoble = 5,17; 
+        // [ERROR] Debes usar punto (.) como separador decimal
+        decimalDoble = 5.17;
+
         enteroLong = 25_369L;
 
-        producto = enteroLong * enteroInt;
+        // producto = enteroLong * enteroInt;
+        // [SOLUCIÓN] CASTING EXPLÍCITO: de tipo long a tipo int
+        producto = (int) enteroLong * enteroInt;
 
-        valorBool = (97 == 'a' == 97);
+        // valorBool = (97 == 'a' == 97);
+        // [ERROR] No se puede aplicar el operador de comparación entre un boolean y un int (97 == 'a' es true -> true == 97 ❌)
+        valorBool = (97 == 'a' && 'a' == 97);
 
-        CONSTANTE1 = 100.3;
+        // CONSTANTE1 = 100.3;
+        // [ERROR] No se puede reasignar una constante
+        double auxVariable = CONSTANTE1;
+        auxVariable = 100.3;
 
+        // [SOLUCIÓN]: CASTING IMPLÍCITO: de tipo char a tipo float
         decimalSimple = 'c';
 
         decimalDoble = 3.2 * 4.7;
 
-        decimalSimple = 9.9 * 4.6;
+        // decimalSimple = (9.9 * 4.6);
+        // [SOLUCIÓN] CASTING EXPLÍCITO: de tipo double a tipo float
+        decimalSimple = (float) (9.9 * 4.6);
 
-        producto = CONSTANTE1 * CONSTANTE2;
+        // producto = CONSTANTE1 * CONSTANTE2;
+        // [SOLUCIÓN] CASTING EXPLÍCITO: de tipo double a tipo int
+        producto = (int) CONSTANTE1 * CONSTANTE2;
 
         decimalDoble = 5.67F;
 
+        // [SOLUCIÓN] CASTING IMPLÍCITO: de tipo int a tipo double
         decimalDoble = 8;
 
+        // [SOLUCIÓN] CASTING IMPLÍCITO: de tipo double a tipo int
         enteroInt = 1 / 2;
     }
 }
--- programacion/tasks/01-tarea-online/src/tarea01/Ejercicio03.java
+++ programacion/tasks/01-tarea-online/src/tarea01/Ejercicio03.java
@@ -2,62 +2,115 @@ package tarea01;
 
 import java.util.Scanner;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+
 /**
  * Ejercicio 3. Validación de contraseñas.
  *
- * @author  David Llopis Laguna
+ * @author David Llopis Laguna
  */
 public class Ejercicio03 {
+
+    private enum PasswordRequirements {
+        LONGITUD_MINIMA("La longitud de la contraseña debe ser, como mínimo, de 12 caracteres."),
+        COMENZAR_VOCAL("Tiene que comenzar con una vocal (mayúscula o minúscula)."),
+        TERMINAR_CONSONANTE("Debe terminar con una consonante (mayúscula o minúscula)."),
+        CARACTERES_ESPECIALES("Tiene que contener, al menos, uno de los siguientes caracteres especiales: arroba ('@'), almohadilla ('#'), barra baja ('_'), punto ('.') o punto y coma (';').");
+
+        private final String description;
+
+        PasswordRequirements(String description) {
+            this.description = description;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+    }
 
     public static void main(String[] args) {
 
         //----------------------------------------------
         //          Declaración de variables 
         //----------------------------------------------
+        // Constantes
+        final int MIN_LENGTH = 12;
+
         // Variables de entrada
-        
-        
+        String userPassword;
+
         // Variables de salida
-        
-        
+        String result;
+        List<String> errors = new ArrayList();
+
         // Variables auxiliares
-        
-        
-        // Clase Scanner para petición de datos de entrada
-        Scanner teclado = new Scanner(System.in);
+        Scanner console = new Scanner(System.in); // Clase Scanner para petición de datos de entrada
+        Predicate<String> hasMinLength = (String password) -> password.length() >= MIN_LENGTH;
+        Predicate<String> beginWithVocal = (String password)
+                -> String.valueOf(password.charAt(0)).matches("[aeiouAEIOU]");
+        Predicate<String> endWithConsonant = (String password)
+                -> String.valueOf(password.charAt(password.length() - 1))
+                        .matches("[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]");
+        Predicate<String> hasSpecialChar = (String password) -> password.matches(".*[@#_.;].*");
+        Predicate<String> isValidPassword = (String password) -> {
+            // Comprobamos si tiene como mínimo 12 caracteres
+            final boolean testResultMinLength = hasMinLength.test(password);
+            
+            // Comprobamos si comienza por una vocal (mayúscula o minúscula)
+            final boolean testResultBeginWithVocal = beginWithVocal.test(password);
+            
+            // Comprobamos si termina con una consonante (mayúscula o minúscula)
+            final boolean testResultEndWithConsonant = endWithConsonant.test(password);
+            
+            // Comprobamos si contiene al menos uno de los caracteres especiales requeridos
+            final boolean testResultHasSpecialChar = hasSpecialChar.test(password);
+
+            // Añadir mensaje de error si no se cumple la condición
+            errors.add(!testResultMinLength ? PasswordRequirements.LONGITUD_MINIMA.getDescription() : null);
+            errors.add(!testResultBeginWithVocal ? PasswordRequirements.COMENZAR_VOCAL.getDescription() : null);
+            errors.add(!testResultEndWithConsonant ? PasswordRequirements.TERMINAR_CONSONANTE.getDescription() : null);
+            errors.add(!testResultHasSpecialChar ? PasswordRequirements.CARACTERES_ESPECIALES.getDescription() : null);
+
+            errors.removeIf(Objects::isNull); // Eliminar cualquier valor nulo de la lista de errores
+
+            return testResultMinLength
+                    && testResultBeginWithVocal
+                    && testResultEndWithConsonant
+                    && testResultHasSpecialChar;
+        };
 
         //----------------------------------------------
         //                Entrada de datos 
         //----------------------------------------------
         System.out.println("VALIDACIÓN DE CONTRASEÑAS");
         System.out.println("-------------------------");
-        
+        System.out.println("Introduzca la contraseña: ");
+        userPassword = console.next();
+        console.close();
 
         //----------------------------------------------
         //                 Procesamiento 
         //----------------------------------------------
-        
         // Cálculo de información auxiliar previa
-        
-        
         // Comprobamos si tiene como mínimo 12 caracteres
-        
-        
         // Comprobamos si comienza por una vocal (mayúscula o minúscula)
-        
-        
         // Comprobamos si termina con una consonante (mayúscula o minúscula)
-        
-        
         // Comprobamos si contiene al menos uno de los caracteres especiales requeridos
-        
-        
+        result = isValidPassword.test(userPassword) ? "VÁLIDA" : "NO VÁLIDA";
+
         //----------------------------------------------
-        //              Salida de resultados 
+        //              Salida de resultados
         //----------------------------------------------
         System.out.println();
         System.out.println("RESULTADO");
         System.out.println("---------");
+        System.out.println("La contraseña es " + result);
+        System.out.println(!errors.isEmpty() ? String.join("\n", errors) : "");
 
+        System.out.println();
+        System.out.println("Fin del programa.");
     }
 }
